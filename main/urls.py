24bacfae82dc587eb6b09b7b89cffe7e363e1df8
MRapricot1 <samueltheo538@gmail.com>
2025-10-29 22:18:40 +0700

    Done Tutor 5

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_news, show_news, show_xml, show_json, show_xml_by_id, show_json_by_id
-from main.views import register
-from main.views import login_user
-from main.views import logout_user
-from main.views import add_employee
-from main.views import edit_news
-from main.views import delete_news
-
+from main.views import show_main, create_news, show_news, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_news, delete_news, add_news_entry_ajax
 
 
 app_name = 'main'
@@ -22,8 +15,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('add-employee/', add_employee, name = 'add_employee'),
     path('news/<uuid:id>/edit', edit_news, name='edit_news'),
     path('news/<uuid:id>/delete', delete_news, name='delete_news'),
-
+    path('create-news-ajax', add_news_entry_ajax, name='add_news_entry_ajax'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,96 +1,17 @@
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.core import serializers
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import NewsForm
 from main.models import News
-from django.http import HttpResponse
-from django.core import serializers
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-import datetime
-from django.http import HttpResponseRedirect
 from django.urls import reverse
-from main.models import Employee
-
-def delete_news(request, id):
-    news = get_object_or_404(News, pk=id)
-    news.delete()
-    return HttpResponseRedirect(reverse('main:show_main'))
-
-def edit_news(request, id):
-    news = get_object_or_404(News, pk=id)
-    form = NewsForm(request.POST or None, instance=news)
-    if form.is_valid() and request.method == 'POST':
-        form.save()
-        return redirect('main:show_main')
-
-    context = {
-        'form': form
-    }
-
-    return render(request, "edit_news.html", context)
-
-def logout_user(request):
-    logout(request)
-    response = HttpResponseRedirect(reverse('main:login'))
-    response.delete_cookie('last_login')
-    return response
-
-def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
-
-      if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
-
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
-
-def register(request):
-    form = UserCreationForm()
-
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Your account has been successfully created!')
-            return redirect('main:login')
-    context = {'form':form}
-    return render(request, 'register.html', context)
-
-def show_json_by_id(request, news_id):
-   try:
-       news_item = News.objects.get(pk=news_id)
-       json_data = serializers.serialize("json", [news_item])
-       return HttpResponse(json_data, content_type="application/json")
-   except News.DoesNotExist:
-       return HttpResponse(status=404)
-
-def show_xml_by_id(request, news_id):
-   try:
-       news_item = News.objects.filter(pk=news_id)
-       xml_data = serializers.serialize("xml", news_item)
-       return HttpResponse(xml_data, content_type="application/xml")
-   except News.DoesNotExist:
-       return HttpResponse(status=404)
-
-def show_json(request):
-    news_list = News.objects.all()
-    json_data = serializers.serialize("json", news_list)
-    return HttpResponse(json_data, content_type="application/json")
-
-def show_xml(request):
-    news_list = News.objects.all()
-    xml_data = serializers.serialize("xml", news_list)
-    return HttpResponse(xml_data, content_type="application/xml")
+import datetime
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -102,13 +23,14 @@ def show_main(request):
         news_list = News.objects.filter(user=request.user)
 
     context = {
-        'npm': '240123456',
-        'name': request.user.username,
-        'class': 'PBP A',
+        'npm' : '2406430104',
+        'name': 'Muhammad Fadhil Al Afifi Fajar',
+        'class': 'PBP C',
         'news_list': news_list,
         'last_login': request.COOKIES.get('last_login', 'Never')
     }
-    return render(request, "main.html",context)
+
+    return render(request, "main.html", context)
 
 def create_news(request):
     form = NewsForm(request.POST or None)
@@ -136,10 +58,129 @@ def show_news(request, id):
 
     return render(request, "news_detail.html", context)
 
-def add_employee(request):
-    employee = Employee.objects.create(
-        name = "Theo Samuel",
-        age = 19,
-        persona = "Saya telah menyelesaikan demo PBP TI2"
+def show_xml(request):
+    news_list = News.objects.all()
+    xml_data = serializers.serialize("xml", news_list)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    news_list = News.objects.all()
+    data = [
+        {
+            'id': str(news.id),
+            'title': news.title,
+            'content': news.content,
+            'category': news.category,
+            'thumbnail': news.thumbnail,
+            'news_views': news.news_views,
+            'created_at': news.created_at.isoformat() if news.created_at else None,
+            'is_featured': news.is_featured,
+            'user_id': news.user_id,
+        }
+        for news in news_list
+    ]
+
+    return JsonResponse(data, safe=False)
+
+def show_xml_by_id(request, news_id):
+    try: 
+        news_item = News.objects.filter(pk=news_id)
+        xml_data = serializers.serialize("xml", news_item)
+        return HttpResponse(xml_data, content_type="application/xml")
+    except News.DoesNotExist:
+       return HttpResponse(status=404)
+
+
+def show_json_by_id(request, news_id):
+    try:
+        news = News.objects.select_related('user').get(pk=news_id)
+        data = {
+            'id': str(news.id),
+            'title': news.title,
+            'content': news.content,
+            'category': news.category,
+            'thumbnail': news.thumbnail,
+            'news_views': news.news_views,
+            'created_at': news.created_at.isoformat() if news.created_at else None,
+            'is_featured': news.is_featured,
+            'user_id': news.user_id,
+            'user_username': news.user.username if news.user_id else None,
+        }
+        return JsonResponse(data)
+    except News.DoesNotExist:
+        return JsonResponse({'detail': 'Not found'}, status=404)
+   
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
+
+@csrf_exempt
+@require_POST
+def add_news_entry_ajax(request):
+    title = strip_tags(request.POST.get("title"))
+    content = strip_tags(request.POST.get("content"))
+    category = request.POST.get("category")
+    thumbnail = request.POST.get("thumbnail")
+    is_featured = request.POST.get("is_featured") == 'on'  # checkbox handling
+    user = request.user
+
+    new_news = News(
+        title=title, 
+        content=content,
+        category=category,
+        thumbnail=thumbnail,
+        is_featured=is_featured,
+        user=user
     )
-    return render(request, 'main.html', {'name': employee.name, 'age': employee.age, 'persona': employee.persona})
+    new_news.save()
+
+    return HttpResponse(b"CREATED", status=201)
